f167da262f1b47ccbe5cb1d841c21090e11728a0
cruzelx <cruze.alex@gmail.com>
2019-02-04 22:30:03 +0545

    Recharge & Payment Completed

--- Arduino_Codes/IntegratedCode/IntegratedCode.ino
+++ Arduino_Codes/IntegratedCode/IntegratedCode.ino
@@ -278,7 +278,7 @@ void loop() {
             Serial.print("Insufficient Money");
             Serial.println();
             }
-        
+            delay(2000);
         
         
         //---------------Deduction End-----------------------------
@@ -286,35 +286,166 @@ void loop() {
             mfrc522.PICC_HaltA();
             // Stop encryption on PCD
             mfrc522.PCD_StopCrypto1();
+            Serial.println("Choose Mode");
+            lcd.setCursor(0,0);
+            lcd.print("A: Payment Mode");
+            Serial.println("A: Payment Mode");
+            lcd.setCursor(0,1);
+            lcd.print("B: Recharge Mode");
+            Serial.println("B: Recharge Mode");
+            //dump_byte_array(key.keyByte,MFRC522::MF_KEY_SIZE);
+            Serial.println();
 
         
         }else if(button == 'B'){
-          if ( ! mfrc522.PICC_IsNewCardPresent())
-          return;
-  
-      // Select one of the cards
-      if ( ! mfrc522.PICC_ReadCardSerial())
-          return;
-  
-      // Show some details of the PICC (that is: the tag/card)
-      Serial.print(F("Card UID:"));
-      //dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-      Serial.println();
-     // Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-     // Serial.println(mfrc522.PICC_GetTypeName(piccType));
-  
-    if (    piccType != MFRC522::PICC_TYPE_MIFARE_MINI
-          &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
-          &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-          Serial.println(F("This sample only works with MIFARE Classic cards."));
-          return;
-      }
+          Serial.println("Chosen Recharge Mode");
           lcd.clear();
-        lcd.print("Recharge Mode");
+          lcd.print("Recharge Mode");
+          delay(1500);
+          lcd.clear();
+          lcd.print("Scan the card");
+          MFRC522::StatusCode status;
+          byte buffer[18];
+          byte size = sizeof(buffer);
+
+
+        while (!mfrc522.PICC_IsNewCardPresent());
+          //Serial.println("Card is read");
+  
+          // Select one of the cards
+          if ( ! mfrc522.PICC_ReadCardSerial())
+              return;
+//          if ( ! mfrc522.PICC_IsNewCardPresent())
+//          return;
+//  
+//        // Select one of the cards
+//        if ( ! mfrc522.PICC_ReadCardSerial())
+//            return;
+    
+        // Show some details of the PICC (that is: the tag/card)
+        //Serial.print(F("Card UID:"));
+        //dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+        Serial.println();
+       // Serial.print(F("PICC type: "));
+        MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+       // Serial.println(mfrc522.PICC_GetTypeName(piccType));
+    
+      if (    piccType != MFRC522::PICC_TYPE_MIFARE_MINI
+            &&  piccType != MFRC522::PICC_TYPE_MIFARE_1K
+            &&  piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+            Serial.println(F("This sample only works with MIFARE Classic cards."));
+            return;
+        }
+          
+           
+          //----------------------Read Current Balance----------------------
+
+          byte MoneyBlockAddr = 8;
+          byte MoneySector = 2;
+          byte MoneyAuthAddr = 11;
+          byte MoneyBlock[16]={0};
+
+          status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, MoneyAuthAddr, &key, &(mfrc522.uid));
+        if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("PCD_Authenticate() failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Error Reading");
+            lcd.setCursor(0,1);
+            lcd.print("Card. Try again");
+            return;
+        }
+        
+     
+        status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, MoneyAuthAddr, &key, &(mfrc522.uid));
+        if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("PCD_Authenticate() failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Error Reading");
+            lcd.setCursor(0,1);
+            lcd.print("Card. Try again");
+            return;
+        }
+        status = (MFRC522::StatusCode) mfrc522.MIFARE_Read(MoneyBlockAddr, buffer, &size);
+        if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("MIFARE_Read() failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Error Reading");
+            lcd.setCursor(0,1);
+            lcd.print("Card. Try again");
+        }
+          //----------------------Read Current Balance End------------------
+
+          //----------------------Update Balance Start----------------------
+
+          int recharge = 250;
+          Serial.print("Buffer val: ");
+          Serial.println(buffer[0]);
+          int sum = buffer[0]+ (250 - buffer[0]);
+          
+          MoneyBlock[0] = sum;
+          Serial.println(MoneyBlock[0]);
+          if(MoneyBlock[0] >= 255 ){
+              Serial.print("Enough Balance");
+              lcd.clear();
+              lcd.print("Enough Balance");
+            }else{
+
+          status = (MFRC522::StatusCode) mfrc522.MIFARE_Write(MoneyBlockAddr, MoneyBlock, 16);
+          if (status != MFRC522::STATUS_OK) {
+              Serial.print(F("MIFARE_Write() failed: "));
+              Serial.println(mfrc522.GetStatusCodeName(status));
+              lcd.clear();
+              lcd.setCursor(0,0);
+              lcd.print("Error Reading");
+              lcd.setCursor(0,1);
+              lcd.print("Card. Try again");
+          }
+          lcd.clear();
+          lcd.print("Recharged: ");
+          lcd.print(250-buffer[0]);
+          delay(1000);
+          lcd.clear();
+          lcd.print("Recharged: ");
+          lcd.print(250-buffer[0]);
+          delay(1000);
+          lcd.clear();
+          lcd.setCursor(0,0);
+          lcd.print("Current Balance");
+          lcd.setCursor(0,1);
+          lcd.print(MoneyBlock[0]);
+          delay(1000);
+          lcd.clear();
+          lcd.print("Return: ");
+          lcd.print(buffer[0]);
+          
+          Serial.print("Current Balance: ");
+          Serial.println(MoneyBlock[0]);
+          Serial.print("Return: ");
+          Serial.println(buffer[0]);
+          
+            }
+          //----------------------Update Balance End-----------------------
+        // Halt PICC
+            mfrc522.PICC_HaltA();
+            // Stop encryption on PCD
+            mfrc522.PCD_StopCrypto1();
+            Serial.println("Choose Mode");
+            lcd.setCursor(0,0);
+            lcd.print("A: Payment Mode");
+            Serial.println("A: Payment Mode");
+            lcd.setCursor(0,1);
+            lcd.print("B: Recharge Mode");
+            Serial.println("B: Recharge Mode");
+            //dump_byte_array(key.keyByte,MFRC522::MF_KEY_SIZE);
+            Serial.println();
           } 
     }
-
 }
 uint8_t getFingerprint(){
             int p = -1;
